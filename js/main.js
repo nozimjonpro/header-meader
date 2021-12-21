7d892b813af62adcf0dad2f98d786c9efe437524
nozimjonpro <nozimjonkeldiyev@yandex.ru>
2021-12-21 11:37:51 +0500

    Add responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const elHambergur = document.querySelector('.hambergur');
 const elHeader = document.querySelector('.header');
+const elClose = document.querySelector('.hambergur--second')
 
 elHambergur.addEventListener('click', ()=>{
+    elHeader.classList.toggle('header--active')
+});
+
+elClose.addEventListener('click', ()=>{
     elHeader.classList.toggle('header--active')
 })
